2ae0409cf61d17fd75a6ef0dc7e58d5bcf21af32
Friedel Ziegelmayer <me@dignifiedquire.com>
2024-11-28 10:51:51 +0100

    feat: simple iroh (#2968)
    ## Description
    
    This is the nexst step to transforming the `iroh` crate, it now is
    purely a reexport crate of the fundamental building blocks
    
    - `iroh-base`
    - `iroh-net`
    - `iroh-router`
    
    ## Breaking Changes
    
    - removed
      - `iroh::node`
      - `iroh::metrics`
      - `iroh::util`
    - `load_secret_key` moved to `iroh_node_utils` to preserve it
    
    ## Notes & open questions
    
    
    
    ## Change checklist
    
    - [ ] Self-review.
    - [ ] Documentation updates following the [style
    guide](https://rust-lang.github.io/rfcs/1574-more-api-documentation-conventions.html#appendix-a-full-conventions-text),
    if relevant.
    - [ ] Tests if relevant.
    - [ ] All breaking changes documented.

--- src/fs.rs
+++ src/fs.rs
@@ -1,18 +1,4 @@
-//! Utilities for building iroh nodes.
-#![deny(missing_docs, rustdoc::broken_intra_doc_links)]
-#![cfg_attr(iroh_docsrs, feature(doc_cfg))]
-#[cfg_attr(iroh_docsrs, doc(cfg(feature = "cli")))]
-#[cfg(feature = "cli")]
-pub mod cli;
-#[cfg_attr(iroh_docsrs, doc(cfg(feature = "config")))]
-#[cfg(feature = "config")]
-pub mod config;
-#[cfg_attr(iroh_docsrs, doc(cfg(feature = "logging")))]
-#[cfg(feature = "logging")]
-pub mod logging;
-pub mod rpc;
-
-pub mod fs;
+//! Utilities for filesystem operations.
 
 use std::path::PathBuf;
 
