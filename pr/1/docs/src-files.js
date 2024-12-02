0d10b6a2cea3581e8601e6e7b4ab9921eccdf2b2
Arqu <Arqu@users.noreply.github.com>
2024-12-02 20:41:46 +0000

    deploy: 1b53059f2d5df5677f50cf41966fa145487e977f

---
+++ pr/1/docs/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["iroh_node_util",["",[["cli",[],["net.rs","node.rs"]],["rpc",[["client",[],["net.rs","node.rs"]],["proto",[],["net.rs","node.rs"]]],["client.rs","proto.rs","server.rs"]]],["cli.rs","config.rs","fs.rs","lib.rs","logging.rs","rpc.rs"]]]\
+]'));
+createSrcSidebar();
